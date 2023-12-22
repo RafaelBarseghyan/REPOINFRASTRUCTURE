8a5d266bff14b47cec5cb626aea040374f6eccdb
RafaelBarseghyan <148814123+RafaelBarseghyan@users.noreply.github.com>
2023-12-22 15:58:44 +0400

    Update 7-nodes.tf

--- 7-nodes.tf
+++ 7-nodes.tf
@@ -42,7 +42,7 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 3
+    desired_size = 2
     max_size     = 5
     min_size     = 0
   }
